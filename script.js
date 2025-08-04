452dc06b22c230e49949a4bf256b329c65586683
Nikhil <nikhilpanthulu26@gmail.com>
2025-08-04 12:09:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-function highlight() {
-    //Write your code here
-
-
+const allStrongWords = document.querySelectorAll("p strong")
+   console.log(allStrongWords)
+    function highlight(){
+    allStrongWords.forEach((element)=>{
+     element.style.color = "rgb(0, 128, 0)"
+    })
 }
 
-
-function return_normal() {
-    //Write your code here
-
-    
+function return_normal(){
+    allStrongWords.forEach((element)=>{
+     element.style.color = "rgb(0, 0, 0)"
+    })
 }
